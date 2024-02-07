ef60c28ce1bf84909ef9f208ac9e63b3bc8ceeeb
ZanMarolt <marolt@zan.sh>
2024-02-07 11:58:29 +0100

    Refactor into signal inputs from decorator inputs

--- apps/04-signal-inputs/src/app/components/01-signal-input.component.ts
+++ apps/04-signal-inputs/src/app/components/01-signal-input.component.ts
@@ -1,23 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [RouterModule],
   selector: 'signal-inputs-default-example',
-  template: `{{ isEven ? 'Even' : 'Odd'}}`,
+  template: `{{ isEven() ? 'Even' : 'Odd'}}`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignalInputComponent {
-    private _num = 0;
-    @Input({ required: true }) set number(val: number) {
-        this.isEven = val % 2 === 0;
-        this._num = val;
-    }
-
-    get number(): number {
-        return this._num;
-    }
-
-    isEven = false;
+  number = input(0, { alias: 'number' });
+  isEven = computed(() => this.number() % 2 === 0);
 }
